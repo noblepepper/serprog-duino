noblepepper/serprog-duino

commit cf1849f7cdf6e5e92b510e3df4e887747c878f0d

Corrected the SPI speed so that the comments are correct (more speed)
Signed-off-by: Sebastian Parborg <darkdefende@gmail.com>
Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

serprog.c
@@ -84,11 +84,12 @@ void setup_spi(void)
 	SPI_PORT &= ~(1<<SS);
 	/* Enable MOSI,SCK,SS as output like on
 	http://en.wikipedia.org/wiki/File:SPI_single_slave.svg */
-	DDR_SPI = (1<<MOSI)|(1<<SCK)|(1<<SS)&~(1<<MISO);
-	/* Enable SPI Master, set the clock to F_CPU / 8 */
+	DDR_SPI = (1<<MOSI)|(1<<SCK)|(1<<SS);
+	/* Enable SPI Master, set the clock to F_CPU / 2 */
 	/* CPOL and CPHA are 0 for SPI mode 0 (see wikipedia) */
 	/* we use mode 0 like for the linux spi in flashrom*/
-	SPCR = (1<<SPE)|(1<<MSTR)&~(1<<CPOL)&~(1<<CPHA)|(1<<SPIE)&~(1<<DORD)&~(1<<SPR0);
+	SPCR = (1<<SPE)|(1<<MSTR);
+    SPSR = (1<<SPI2X);
 }
 
 char readwrite_spi(char c)

Makefile
@@ -1,5 +1,5 @@
 CC=avr-gcc
-CFLAGS=-Wall -Os -DF_CPU=$(F_CPU) -mmcu=$(MCU)
+CFLAGS=-Wall -O3 -DF_CPU=$(F_CPU) -mmcu=$(MCU)
 MCU=atmega328p
 F_CPU=16000000L
 

commit 198baa74cbe2d3812c9f2f2cc474f0c61edc6d7f

return a big bogus Q_SERBUF value like what the documentation asks for
The documentation says:
  0x04 (Q_SERBUF):
    If the programmer has a guaranteed working flow control,
    it should return a big bogus value - eg 0xFFFF.

That change seem to fix a strange bug (introduced in
  cf1849f7cdf6e5e92b510e3df4e887747c878f0d )
  where flashrom expected to read 2 more bytes and waited indefinitely.

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

serprog.c
@@ -175,8 +175,8 @@ void handle_command(unsigned char command)
 			break;
 		case S_CMD_Q_SERBUF:
 			putchar_uart(S_ACK);
-			putchar_uart(0x00);
-			putchar_uart(0x00);
+			putchar_uart(0xFF);
+			putchar_uart(0xFF);
 			break;
 		case S_CMD_Q_BUSTYPE:
 			putchar_uart(S_ACK);